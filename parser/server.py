5a459a6d5b105d9c445502b0590b54ad81afd251
Aleksei M <for.shit@yahoo.com>
2024-05-02 00:14:24 +0530

    add chrome and chromeDriver

--- parser/server.py
+++ parser/server.py
@@ -1,10 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask
+from controller import get_steam_items
+from proxy import fetch_proxies_from_2ip
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return jsonify({"message": "Hello, world!"})
+@app.route('/items/<int:appid>')
+def items(appid):
+    return get_steam_items(appid)
+
+@app.route('/proxy')
+def proxy():
+    return fetch_proxies_from_2ip()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
