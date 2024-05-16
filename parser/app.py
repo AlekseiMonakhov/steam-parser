4d250dc7e34d14b51df5ba820d2183b8d62e1fc9
Aleksei M <lexmonakhov@gmail.com>
2024-05-17 02:34:41 +0530

    add parsing sheduler

--- parser/app.py
+++ parser/app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 from dotenv import load_dotenv
 from steam_service.steam_item_service import SteamItemService
 from config import Config
+from apscheduler.schedulers.background import BackgroundScheduler
 
 logging.basicConfig(level=logging.INFO)
 
@@ -10,11 +11,26 @@ load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/items/<int:appid>')
-def items(appid):
+APP_IDS = [570, 730, 578080]
+
+def run_service(appid):
     service = SteamItemService(Config.API_KEY, appid)
     service.run()
+
+@app.route('/items/<int:appid>')
+def items(appid):
+    run_service(appid)
     return jsonify({"status": "Completed processing items"}), 200
 
+def scheduled_tasks():
+    for appid in APP_IDS:
+        run_service(appid)
+
 if __name__ == "__main__":
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(scheduled_tasks, 'interval', hours=12)
+    scheduler.start()
+
+    scheduled_tasks()
+
     app.run(host='0.0.0.0', port=5000)
